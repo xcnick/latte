91d8838941b60d8b7e13551d74fa118297a072f8
xcnick <xcnick0412@gmail.com>
2024-09-04 09:53:34 +0000

    使用原生 actions 方案替换原有基于 docker 的方案

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-set -xe
-
-SRC_ROOT=$1
-cd ${SRC_ROOT}
-cmake -S . -B build
-cmake --build build -j
--- scripts/build_test.sh
+++
@@ -1,7 +0,0 @@
-set -xe
-
-SRC_ROOT=$1
-cd ${SRC_ROOT}
-cmake -S . -B build
-cmake --build build -j
-./build/src/latte/test/test_latte --gtest_filter="-*GPU*"
